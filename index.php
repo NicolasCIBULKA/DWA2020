f802995e0e9bcbfa1a7fe63a7cc1b8739cd5c017
NicolasCIBULKA <nico.cibulka@gmail.com>
2020-10-28 14:42:22 +0100

    debut index et templates

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 
 <body>
 	<?php include("templates/header.inc.php"); ?>
-	
+	<form method="post" action="functions/connexion.php">
+		<p>
+			<label for="identifiant">Identifiant : </label>
+			<input type="text" name="identifiant" id="identifiant"/>
+		</p>
+		<p>
+			<label for="mdp">Mot de passe : </label>
+			<input type="password" name="mdp" id="mdp"/>
+		</p>
+		<input type="submit" value="Se connecter">
+	</form>
+	<p><a href="#">Creer un compte</a></p>
 
 	<?php include("templates/footer.inc.php"); ?>
 </body>
--- templates/header.inc.php
+++ templates/header.inc.php
@@ -2,5 +2,4 @@
 		<h1>Blabla't</h1>
 		<h2>Une nouvelle façon de communiquer</h2>
 
-		
 </header>
