c3adc1cb1328044c7c961e14bb34b7308ac31c98
NicolasCIBULKA <nico.cibulka@gmail.com>
2020-10-28 15:20:19 +0100

    suite ajout fichiers/dossiers

---
+++ conversations.php
@@ -0,0 +1,20 @@
+<!--
+ Page affichant l'ensemble des conversations de l'utilisateur
+ Chaque conversation sera cliquable et amenera vers ladite conversation
+
+ -->
+
+ <?php include ("templates/head.inc.php"); ?>
+<body>
+	<?php include ("templates/header.inc.php"); ?>
+
+	<session>
+		<h2>Vos Conversations</h2>
+		<article>
+			<h3>Conversation</h3>
+		</article>
+	</session>
+
+	<?php include ("templates/footer.inc.php"); ?>
+</body>
+</html>
