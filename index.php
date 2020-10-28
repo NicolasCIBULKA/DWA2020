605db849c6c2eeb114f78d30f4e72b6c0f7ddd66
NicolasCIBULKA <nico.cibulka@gmail.com>
2020-10-28 14:26:20 +0100

    ajout classes

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("templates/head.inc.php"); ?>
+
+<body>
+	<?php include("templates/header.inc.php"); ?>
+	
+
+	<?php include("templates/footer.inc.php"); ?>
+</body>
+</html>
---
+++ templates/header.inc.php
@@ -0,0 +1,6 @@
+<header>
+		<h1>Blabla't</h1>
+		<h2>Une nouvelle façon de communiquer</h2>
+
+		
+</header>
