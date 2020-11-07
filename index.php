cde2881e67dc754e06a73d8ae1f337d6f9be65c6
knbernard <60677447+knbernard@users.noreply.github.com>
2020-11-07 16:28:35 +0100

    fieozfa

--- index.php
+++ index.php
@@ -6,19 +6,41 @@ Page d'accueil permettant la connexion ainsi que la création de compte
 
 <body>
 	<?php include("templates/header.inc.php"); ?>
-	<form method="post" action="functions/connexion.php">
-		<p>
-			<label for="identifiant">Identifiant : </label>
-			<input type="text" name="identifiant" id="identifiant"/>
-		</p>
-		<p>
-			<label for="mdp">Mot de passe : </label>
-			<input type="password" name="mdp" id="mdp"/>
-		</p>
-		<input type="submit" value="Se connecter">
-	</form>
-	<p><a href="#">Creer un compte</a></p>
-
+	<div class="container-fluid">
+		<div class="row">
+			<article class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+				<h2 class="text-center slogant"> Redécouvrez comment discuter ! </h2>
+				<div class="modal-dialog modal-login shadow p-3 mb-5 bg-white">
+					<div class="modal-content">
+						<div class="modal-header">
+							<div class="avatar">
+								<img src="images/avatar-2.png" alt="Avatar">
+							</div>				
+							<h4 class="modal-title">Connexion</h4>	
+						</div>
+					<div class="modal-body">
+						<form method="post" action="functions/connexion.php">
+							<div class="form-group">
+								<input class="form-control" type="text" name="identifiant" id="identifiant" placeholder="Identifiant" required="required"/>
+							</div>
+							<div class="form-group">
+								<input class="form-control" type="password" name="mdp" id="mdp" placeholder="Mot de passe" required="required"/>
+							</div>
+							<div class="form-group">
+								<input class="btn btn-primary btn-lg btn-block login-btnn" type="submit" value="Se connecter">
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<a href="#">Créer un compte</a>
+					</div>
+				</div>
+			</article>
+			<article class="col-xs-6 col-sm-6 col-md-6 col-lg-6 pr-0 pt-3">
+				<img  class="img-fluid" src="images/womenChatting.jpg"  alt="Discussion">
+			</article>
+		</div>
+	</div>
 	<?php include("templates/footer.inc.php"); ?>
 </body>
 </html>
--- templates/head.inc.php
+++ templates/head.inc.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr"/>
+<html lang="fr">
 <head>
 	<meta charset="UTF-8"/>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
