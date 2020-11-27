982b8588d8ac853acd14713ca9d8fc948aa8c152
knbernard <60677447+knbernard@users.noreply.github.com>
2020-11-27 14:41:39 +0100

    fieozfa

--- templates/header.inc.php
+++ templates/header.inc.php
@@ -1,9 +1,13 @@
-<header class="row">
-	<div class="col-1">
-		<img class="img-responsive" src="images/logo.png" alt="Logo" style="width:100%">
+<header>
+	<div class="container-fluid">
+		<div class="text-center">
+			<img class="center-block" src="images/logo.png" alt="Logo" style="width:10%">
+		</div>
 	</div>
-	<div class="col-11">
-		<h1>Blabla't</h1>
-		<h2 id="undername">Une nouvelle façon de communiquer</h2>
+	<div class="row">
+		<div class="col-12">
+			<h1> Blabla't </h1>
+			<h2 id="undername">Une nouvelle façon de communiquer</h2>
+		</div>
 	</div>
 </header>
