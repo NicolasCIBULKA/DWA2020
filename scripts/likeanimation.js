e6073bbae1eca5763fa196f88e799af0f360b589
Ashanth <ashanth92@gmail.com>
2020-11-27 16:12:26 +0100

    des likes encore et toujours des likes

--- scripts/likeanimation.js
+++ scripts/likeanimation.js
@@ -1,13 +1,8 @@
-/*parametre 1 : class du bouton
-pour l'instant marche avec des id, arrive pas a generaliser
-*/
-$(".likes svg").click(function(){
-  var likeid = $(this).attr("id");
-
- likeMethode(likeid);
+$(".likebutton svg").click(function(){
+  	var likeid = $(this).attr("id");
+	likeMethode(likeid);
 });
 
-//parametre :id du svg
 function likeMethode ($likee) {
 
 if ($('#'+$likee).attr('fill') == 'white'){
