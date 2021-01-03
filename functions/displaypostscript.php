8eddfc5030048622867c08e4b1e5517eb34d6c2c
knbernard <60677447+knbernard@users.noreply.github.com>
2021-01-03 19:21:19 +0100

    fieozfa

--- functions/displaypostscript.php
+++ functions/displaypostscript.php
@@ -9,14 +9,14 @@ if(isset($_GET["iduser"])) {
 	$postIndex = 0;
 	$comIndex = 0;
 	$bdd = BDConnect();
-	$followedReq = $bdd->prepare("SELECT idFollowed FROM Follow where idFollower = ?");
+	$followedReq = $bdd->prepare("SELECT id_followed FROM Follow where id_follower = ?");
 	$followedReq->execute(array($_GET["iduser"]));
 	if($followedReq->rowCount() == 0) {
 		echo "<p class=\"text-center\">Vous ne suivez aucune personne</p>";
 	}
 	else {
-		while(($followedRow = $followedReq->fetch() && ($followedIndex < 20)) {
-			$postReq = $bdd->prepare("(SELECT * FROM Post where idWriter = ?) UNION (SELECT * FROM Post where idWriter = ?)");
+		while(($followedRow = $followedReq->fetch() && ($followedIndex < 20))) {
+			$postReq = $bdd->prepare("(SELECT * FROM Post WHERE id_writer = ?) UNION (SELECT * FROM Post WHERE id_writer = ?)");
 			$postReq->execute(array($followedRow[1],$_GET["iduser"]));
 			while(($postRow = $postReq->fetch()) && ($postIndex < 20)) {
 				$post = new Post($postRow[2],$postRow[3],false,$postRow[1]);
@@ -38,6 +38,7 @@ if(isset($_GET["iduser"])) {
 		$followedIndex++;
 		}
 	}
+}
 else {
 	echo "<p class=\"text-center\">Erreur lors de l'affichage des posts ...</p>";
 }
